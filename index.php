62b03de40c23b4477c5d65c3c58d86c049987e06
Ivan Yared Cambranis <l21530246@cancun.tecnm.mx>
2024-11-20 22:59:22 -0500

    Update index.php

--- index.php
+++ index.php
@@ -20,4 +20,5 @@
   <a href = "PRACTICA 6.php"> Practica6 php </a> <br>
   <a href = "image.png"> Practica7 php </a> <br>
   <a href = "PHOTO-2024-11-20-14-50-07.jpg"> Practica8 php </a> <br>
+  <a href = "PRACTICA 9.php"> Practica9 php </a> <br>
 </body>
